cdc026292d2389a2d09e78aec86ac656ecc93b65
QliXeD [Ezequiel Brizuela] <qlixed@gmail.com>
2017-03-21 21:13:28 -0300

    - Correct some output of the coretest.py to make it more easy to use and understand

--- coretest.py
+++ coretest.py
@@ -14,26 +14,33 @@ def supersecretinfogenerator():
     with open('supersecretinfo.txt', 'w') as f:
         f.write(srot13)
 
-
-p = multiprocessing.Process(target=supersecretinfogenerator)
-p.start()
-p.join()
-with open("supersecretinfo.txt", 'r') as f:
-    sfromfile = f.read()
-print ('The super secret info is:', sfromfile)
-s = codecs.encode(sfromfile, 'rot_13')
-print("Now go and check all out, I will wait here")
-print(""" To do it:
+if __name__ == "__main__":
+    p = multiprocessing.Process(target=supersecretinfogenerator)
+    p.start()
+    p.join()
+    with open("supersecretinfo.txt", 'r') as f:
+        sfromfile = f.read()
+    print ('The super secret info is:', sfromfile)
+    s = codecs.encode(sfromfile, 'rot_13')
+    print("Now go and check all out, I will wait here")
+    print(""" To do it:
 1) Run gdb and attach it to my process ({pid})
-      gdb -x generate-core-file {pid}
+      gdb -ex generate-core-file -ex detach -ex quit -p {pid}
    This will generate the file core.{pid}
 2) Run:
       strings core.{pid} | grep 'your secret strings'
       Now you can see the super secret info decoded! :o
 """.format(pid=os.getpid()))
-input()
-print ("Now, we are going to repeat this, but let me first wipe it")
-memwiper.wipeit(s)
-print("Now go and check all out, I will wait here")
-input()
-print ("Magic?!")
+    input()
+    print ("Now, we are going to repeat this, but let me first wipe it")
+    memwiper.wipeit(s)
+    print("""Now go and check all out, I will wait here.
+Again:
+1) Run gdb and attach it to my process ({pid})
+      gdb -ex generate-core-file -ex detach -ex quit -p {pid}
+   This will generate the file core.{pid}
+2) Run:
+      strings core.{pid} | grep 'your secret strings'
+""".format(pid=os.getpid()))
+    input()
+    print ("Magic?!")
