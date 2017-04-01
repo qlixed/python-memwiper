549dc1f9b20020e86ef96b944d68a5fa2c086e21
QliXeD [Ezequiel Brizuela] <qlixed@gmail.com>
2017-04-01 17:52:22 -0300

    [TEST] conftest.py: If method don't need fixture, skip the fixture assignation to avoid errors [TEST] test_memwiper.py: Add test for Big (65k+) and Huge (100M+) strings

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,11 +5,13 @@ def pytest_addoption(parser):
     parser.addoption("--testfile",
                      action="append",
                      default=['tests/basictest.txt'],
-        help="File used to load strings for test, "
+                     help="File used to load strings for test, "
                      "we provide a default (basictest.txt)")
 
 
 def pytest_generate_tests(metafunc):
+    if 'teststring' not in metafunc.fixturenames:
+        return
     strings = None
     with open(metafunc.config.option.testfile[0], 'r', encoding='utf_8') as f:
         strings = f.readlines()
--- tests/test_memwiper.py
+++ tests/test_memwiper.py
@@ -22,3 +22,21 @@ def test_multiple_references(teststring):
     assert len(s2) == lenteststring
     assert s1 == s2
     assert len(s1) == len(s2)
+
+def test_long_string_wipe():
+    """
+    Test long (65k+) string wipe
+    """
+    s1='C0FFEE'*65536
+    lens1=len(s1)
+    assert memwiper.wipeit(s1)==''
+    assert len(s1) == lens1
+
+def test_huge_string_wipe():
+    """
+    Test HUGE (100M+) string wipe
+    """
+    s1='C0FEE'*(100 * 1024 * 1024)
+    lens1=len(s1)
+    assert memwiper.wipeit(s1)==''
+    assert len(s1) == lens1
